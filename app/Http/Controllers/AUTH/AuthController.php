eead3f65ebe29a2a799d2e918b7b827636544501
Argi2912 <argimirojrodriguezrr@gmail.com>
2024-10-29 22:55:28 -0400

    create register request for better validation

--- app/Http/Controllers/AUTH/AuthController.php
+++ app/Http/Controllers/AUTH/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\AUTH;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AUTH\LoginRequest;
+use App\Http\Requests\AUTH\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -14,17 +15,12 @@ use function Laravel\Prompts\error;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validated = $request->validate([
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed'
-        ]);
-
+    public function register(RegisterRequest $request){
+        
         $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated["password"])
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
 
         $token = JWTAuth::fromUser($user);
